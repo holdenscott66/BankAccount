c4b2bc4883157c19d1bf354c766c39f750af6959
Ayryus <35973756+Ayryus@users.noreply.github.com>
2018-02-22 11:31:30 -0700

    Add files via upload

--- src/BankApplication.java
+++ src/BankApplication.java
@@ -1,39 +1,107 @@
+import javafx.stage.Stage;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
-public class BankApplication extends Application implements EventHandler<ActionEvent>{
+public class BankApplication extends Application {
 	
-	Customer c1 = new Customer("John", 12345);
+	// NOT DONE COMMENTING YET, WILL DO IT NEXT WEEK 
+	
+	
+	//declaring the stage and scene
+	Stage window;
+	Scene scene1;
+
+	//declaring the execute button
+	Button executeButton;
+	
+	//declare the customer and the savings account amount
+	Customer c1 = new Customer("John", 314159);
 	SavingsAccount s1 = new SavingsAccount(c1, 150.00);
-	Button b1;
+
 	
-	public static void main(String[] args) {
-		launch(args);	
-	}
-	@Override
+	
+	
 	public void start(Stage primaryStage) throws Exception {
-		primaryStage.setTitle("Bank Application");
-		b1 = new Button("Execute");
-		b1.setOnAction(e -> System.out.println());
+
+		window = primaryStage;
 		
-		StackPane layout = new StackPane();
-		layout.getChildren().add(b1);
+
+		VBox layout1 = new VBox(15);
 		
-		layout.getInputMethodRequests().getSelectedText();
-		Scene scene = new Scene(layout, 300, 400);
-		primaryStage.setScene(scene);
-		primaryStage.show();
+			Label customerNameLabel = new Label("Customer Name: " + c1.getName());
+	
+			String stringID = Integer.toString(c1.getID());
+			Label customerIDLabel = new Label("Account ID #" + (stringID));
+	
+
+	
+		//horizontal placements for the text boxes
+		HBox layout2 = new HBox(15);
+		
+			TextField depositTextField = new TextField();
+			depositTextField.setPromptText("amt to deposit");
+		
+			TextField withdrawTextField = new TextField();
+			withdrawTextField.setPromptText("amt to withdraw");
+		
+			
+		VBox layout3 = new VBox(15);
+		
+			Label balanceLabel = new Label("Current Balance $" + s1.getBalance());
+			
+			
+			executeButton = new Button();
+			executeButton.setText("Execute");
+			
+			executeButton.setOnAction(e -> {
+			try { 
+				if (withdrawTextField.getText().isEmpty()) 
+					{
+					 
+					}
+				else 
+					{
+						double withdraw = Double.parseDouble(withdrawTextField.getText()); 
+						s1.withdraw(withdraw);
+						balanceLabel.setText("Current Balance $" + s1.getBalance());
+						depositTextField.clear();
+					}
+				
+				if (depositTextField.getText().isEmpty()) {
+					withdrawTextField.clear();
+					depositTextField.clear();
+					}
+				else 
+					{
+						double deposit = Double.parseDouble(depositTextField.getText()); 
+						s1.deposit(deposit);
+						balanceLabel.setText("Current Balance $" + s1.getBalance());
+						depositTextField.clear();
+					}
+			}
+			
+			catch(Exception a) 
+			{
+				depositTextField.clear();
+				depositTextField.clear();
+	
+			}
+		});
+
+
+		layout3.getChildren().addAll(executeButton, balanceLabel);
+		layout2.getChildren().addAll(depositTextField, withdrawTextField);
+		layout1.getChildren().addAll(customerNameLabel, customerIDLabel, layout2, layout3);
 		
 		
-	}
-	@Override
-	public void handle(ActionEvent arg0) {
-		// TODO Auto-generated method stub
-		
+		scene1 = new Scene(layout1, 400,400);	
+		window.setTitle("Bank Application");
+		window.setScene(scene1);
+		window.show();
 	}
 }
